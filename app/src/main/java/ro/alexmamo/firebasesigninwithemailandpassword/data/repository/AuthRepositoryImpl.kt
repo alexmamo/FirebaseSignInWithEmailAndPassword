9ad5aadcd5026c4daf79522d6acfeddffbfc6b09
Alex Mamo <alexmamo@gmail.com>
2023-07-11 10:10:38 +0300

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firebasesigninwithemailandpassword/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/ro/alexmamo/firebasesigninwithemailandpassword/data/repository/AuthRepositoryImpl.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.tasks.await
 import ro.alexmamo.firebasesigninwithemailandpassword.domain.model.Response.Failure
 import ro.alexmamo.firebasesigninwithemailandpassword.domain.model.Response.Success
-import ro.alexmamo.firebasesigninwithemailandpassword.domain.repository.*
+import ro.alexmamo.firebasesigninwithemailandpassword.domain.repository.AuthRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -22,62 +22,50 @@ class AuthRepositoryImpl @Inject constructor(
 
     override suspend fun firebaseSignUpWithEmailAndPassword(
         email: String, password: String
-    ): SignUpResponse {
-        return try {
-            auth.createUserWithEmailAndPassword(email, password).await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    ) = try {
+        auth.createUserWithEmailAndPassword(email, password).await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
-    override suspend fun sendEmailVerification(): SendEmailVerificationResponse {
-        return try {
-            auth.currentUser?.sendEmailVerification()?.await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    override suspend fun sendEmailVerification() = try {
+        auth.currentUser?.sendEmailVerification()?.await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
     override suspend fun firebaseSignInWithEmailAndPassword(
         email: String, password: String
-    ): SignInResponse {
-        return try {
-            auth.signInWithEmailAndPassword(email, password).await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    ) = try {
+        auth.signInWithEmailAndPassword(email, password).await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
-    override suspend fun reloadFirebaseUser(): ReloadUserResponse {
-        return try {
-            auth.currentUser?.reload()?.await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    override suspend fun reloadFirebaseUser() = try {
+        auth.currentUser?.reload()?.await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
-    override suspend fun sendPasswordResetEmail(email: String): SendPasswordResetEmailResponse {
-        return try {
-            auth.sendPasswordResetEmail(email).await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    override suspend fun sendPasswordResetEmail(email: String) = try {
+        auth.sendPasswordResetEmail(email).await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
     override fun signOut() = auth.signOut()
 
-    override suspend fun revokeAccess(): RevokeAccessResponse {
-        return try {
-            auth.currentUser?.delete()?.await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
-        }
+    override suspend fun revokeAccess() = try {
+        auth.currentUser?.delete()?.await()
+        Success(true)
+    } catch (e: Exception) {
+        Failure(e)
     }
 
     override fun getAuthState(viewModelScope: CoroutineScope) = callbackFlow {
